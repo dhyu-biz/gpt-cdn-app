4971bacd81cb5cbed2f82d94573c253238346326
YUDAHAN <dbekgks98@gmail.com>
2025-07-17 22:21:56 +0900

    iframe props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 export default function App() {
-  return <div>Hello from CDN!</div>;
+  // index.js (iframe 내부 진입점)
+  const query = new URLSearchParams(window.location.search);
+  const color = query.get("color"); // red
+  const userId = query.get("userId"); // 123
+
+  return (
+    <div>
+      <h1>Hello from CDN!</h1>
+      <p>This is a paragraph</p>
+      <p>color: {color}</p>
+      <p>userId: {userId}</p>
+    </div>
+  );
 }
