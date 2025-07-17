d1e9f6c90bfb922a7ace25418c2a293469633521
YUDAHAN <dbekgks98@gmail.com>
2025-07-17 22:25:01 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-export default function App() {
-  // index.js (iframe 내부 진입점)
-  const query = new URLSearchParams(window.location.search);
-  const color = query.get("color"); // red
-  const userId = query.get("userId"); // 123
+export default function App({ color, userId }) {
+  
 
   return (
     <div>
       <h1>Hello from CDN!</h1>
-      <p>This is a paragraph</p>
       <p>color: {color}</p>
       <p>userId: {userId}</p>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// index.js (iframe 내부 진입점)
+const query = new URLSearchParams(window.location.search);
+const color = query.get("color"); // red
+const userId = query.get("userId"); // 123
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App color={color} userId={userId}/>);
